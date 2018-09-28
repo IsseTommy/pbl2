a22968a1c3ed569763bcc419fdb453bbe9b0c087
卓怡安 <choian@chos-macbook-pro.local>
2018-09-28 14:18:01 +0900

    hiiiii

---
+++ pbl1/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  pbl1
+//
+//  Created by 卓怡安 on 2018/09/28.
+//  Copyright © 2018 tommy. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
