61c943d2d258e4e7e570b9b1838ce7ef9c4674a0
カン・ヘジュン <kanghaejoon@kangui-macbook-air.local>
2018-09-28 13:11:46 +0900

    ho

--- pbl1/ViewController.swift
+++ pbl1/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     // comment by htakada
     //This is Tommy
     //im joon!!
